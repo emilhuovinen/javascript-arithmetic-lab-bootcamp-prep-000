2a33f58c3c0dacc9be77a79bdb0f8cc10fe1110d
emil.huo@gmail.com <emil.huo@gmail.com>
2018-09-11 18:47:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-var a, b
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
 
 function add(a, b) {
-  a + b
+  (a + b)
 }
 
 function subtract(a, b) {
@@ -21,3 +16,4 @@ function divide(a, b) {
   console.log(a / b);
 }
 
+add(1,2);
